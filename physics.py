c8bdad4907be4c8cfe26d4f4007f1a7ff87b9443
Rasmus Kramare <rasmus.kramare@live.se>
2024-04-13 11:50:49 +0200

    TopHat PhysicS

--- physics.py
+++ physics.py
@@ -3,6 +3,32 @@ import numpy as np
 
 th = TopHat()
 
+g = 9.8
+
 
 def update(dt):
-    pass
+    Ftot = np.zeros(3)
+    Ttot = np.zeros(3)
+
+    # speed and inertia
+    v = th.lin_mom / th.mass
+    inertia_loc = th.rot @ th.inertia @ th.rot.T
+    omega = inertia_loc @ th.ang_mom
+
+    # forces
+    F = np.array([0, 0, -g]) * th.mass
+    Floc = th.pos
+    Ftot += F
+    Ttot += np.cross(Floc, F)
+
+    # derivative
+    Rd = np.cross(np.eye(3), omega * dt) @ th.rot
+
+    # update
+    th.pos += v * dt
+    th.rot += Rd + dt
+    th.lin_mom += Ftot * dt
+    th.ang_mom += Ttot * dt
+
+    # ortnormalize
+    th.rot = np.linalg.qr(th.rot)[0]
--- tophat.py
+++ tophat.py
@@ -4,9 +4,10 @@ import numpy as np
 class TopHat:
     def __init__(self):
         self.mass = 1
-        self.inertia = np.zeros((3, 3))
+        self.inertia = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+        self.inertia_inv = np.linalg.inv(self.inertia)
         self.center = np.zeros(3)
         self.pos = np.zeros(3)
         self.rot = np.zeros((3, 3))
         self.lin_mom = np.zeros(3)
-        self.ang_mom = np.zeros((3, 3))
+        self.ang_mom = np.zeros(3)
