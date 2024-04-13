ce7787c8b69b5c7da2dea589e0bbe74ead121149
Rasmus Kramare <rasmus.kramare@live.se>
2024-04-13 21:13:11 +0200

    Physics changes. Snapping and collison kinda good

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ursina import *
 import physics
 from scipy.spatial.transform import Rotation as R
 from ursina.models.procedural.cylinder import Cylinder
+import numpy as np
 
 # create a window
 app = Ursina()
@@ -16,14 +17,26 @@ app = Ursina()
 # 'scale_y=2' tells us how big the entity should be in the vertical axis, how tall it should be.
 # in ursina, positive x is right, positive y is up, and positive z is forward.
 
-player = Entity(model=Cylinder(radius=1.2), color=color.orange, scale_y=2)
-box = Entity(model="cube", color=color.red, scale=(10, 0.5, 10), position=(0, -2, 0))
+physics.th.rot = R.from_euler("z", 30, degrees=True).as_matrix()
+physics.th.lin_mom = np.array([3.0, 0.0, 0.0])
+
+player = Entity(model=Cylinder(radius=1.2, start=-0.5), color=color.orange, scale_y=2)
+box = Entity(model="cube", color=color.red, scale=(10, 0.5, 10), position=(0, -2.5, 0))
 
 # create a function called 'update'.
 # this will automatically get called by the engine every frame.
 
+# print(R.from_euler("z", 180, degrees=True).as_matrix())
+# print(R.from_euler("z", 0, degrees=True).as_matrix())
+# exit()
+
+inc = 0
+
 
 def update():
+    # global inc
+    # inc += 1
+    # physics.th.rot = R.from_euler("z", inc, degrees=True).as_matrix()
     physics.update(1 / 60)
     player.position = physics.th.pos
     player.rotation = R.from_matrix(physics.th.rot).as_euler("xyz", degrees=True)
@@ -38,8 +51,8 @@ def update():
 
 def input(key):
     if key == "space":
-        physics.th.pos = Vec3(0, 0, 0)
-        physics.th.lin_mom = Vec3(0, 0, 0)
+        physics.th.pos = np.array([0.0, 0.0, 0.0])
+        physics.th.lin_mom = np.array([0.0, 0.0, 0.0])
 
 
 # start running the game
